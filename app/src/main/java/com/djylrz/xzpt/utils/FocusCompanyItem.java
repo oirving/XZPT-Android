347674074aec01a156ab94019cffe5f005c82221
oirving <865306541@qq.com>
2019-04-27 21:46:38 +0800

    修复"我的->关注公司"打开没有显示的问题

--- app/src/main/java/com/djylrz/xzpt/utils/FocusCompanyItem.java
+++ app/src/main/java/com/djylrz/xzpt/utils/FocusCompanyItem.java
@@ -4,6 +4,11 @@ public class FocusCompanyItem {
     int companyLoge;
     String companyName;
 
+    /**
+     *
+     * @param companyLoge 企业logo
+     * @param companyName 企业名称
+     */
     public FocusCompanyItem(int companyLoge, String companyName) {
         this.companyLoge = companyLoge;
         this.companyName = companyName;
