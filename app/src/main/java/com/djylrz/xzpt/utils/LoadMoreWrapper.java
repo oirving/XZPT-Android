21b67af80c1fa357328f76fd3320bd26f0acfa8a
supermingjun <544002828@qq.com>
2019-04-30 01:56:26 +0800

    修复企业端Toolbar高度不一致问题;修复企业端在已经登录时，通过返回键会返回到登录界面bug;新增在企业端首页，需要连续点击两次返回才能退出应用;对部分页面菜单图标进行调整，使其符合MD风格

--- app/src/main/java/com/djylrz/xzpt/utils/LoadMoreWrapper.java
+++ app/src/main/java/com/djylrz/xzpt/utils/LoadMoreWrapper.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
