dc05a53a6e3557ef46c39e8f2ec6a58332c2e334
oirving <865306541@qq.com>
2019-05-03 01:59:30 +0800

    面试指导

---
+++ app/src/main/java/com/djylrz/xzpt/utils/InterviewTipsAdapter.java
@@ -0,0 +1,57 @@
+package com.djylrz.xzpt.utils;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.djylrz.xzpt.R;
+
+import java.util.List;
+
+/**
+ * @author oirving
+ * 招聘日历适配器
+ */
+
+
+public class InterviewTipsAdapter extends RecyclerView.Adapter<InterviewTipsAdapter.ViewHolder> {
+
+    private List<InterviewTipsItem> interviewTipsItems ;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;//标题
+
+        public ViewHolder(View view) {
+
+            super(view);
+            title = (TextView) view.findViewById(R.id.interview_title);
+        }
+    }
+
+    public InterviewTipsAdapter (List<InterviewTipsItem> interviewTipsItems){
+        this.interviewTipsItems = interviewTipsItems;
+    }
+    @NonNull
+    @Override
+    public InterviewTipsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.interview_item,viewGroup,false);
+        ViewHolder holder = new InterviewTipsAdapter.ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder  viewHolder, int position) {
+        InterviewTipsItem interviewTipsItem = interviewTipsItems.get(position);
+        viewHolder.title.setText(interviewTipsItem.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return interviewTipsItems.size();
+    }
+}
