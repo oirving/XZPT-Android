fce9a1be0511a373b191e50d773794c2dc5b6813
supermingjun <544002828@qq.com>
2019-05-03 22:56:15 +0800

    Revert "Merge pull request #54 from oirving/master"
    This reverts commit ed51f8a1850119c5330599ddd9abee92e5d6556e, reversing
    changes made to 91f0d864a7c2b2725ba59d621edff80edb58d04b.

--- app/src/main/java/com/djylrz/xzpt/fragment/FragmentFindJob.java
+++ app/src/main/java/com/djylrz/xzpt/fragment/FragmentFindJob.java
@@ -11,9 +11,6 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 
 import com.djylrz.xzpt.R;
 import com.flyco.tablayout.SegmentTabLayout;
@@ -21,13 +18,10 @@ import com.flyco.tablayout.listener.OnTabSelectListener;
 
 import java.util.ArrayList;
 
-public class FragmentFindJob extends Fragment implements View.OnClickListener{
+public class FragmentFindJob extends Fragment {
     private Context mContext = getContext();
     private String[] mTitles = {"推荐","热门","联系"};
     private View mDecorView;
-    private ImageView search;
-    private EditText searchEditText;
-    private Button searchButton;
     private SegmentTabLayout mTabLayout;
     private ArrayList<Fragment> mFragments;
     private static final String TAG = "FragmentFindJob";
@@ -40,10 +34,7 @@ public class FragmentFindJob extends Fragment implements View.OnClickListener{
         for (String title : mTitles) {
             mFragments.add(RecommendCardFragment.getInstance(title));
         }
-        search = (ImageView) mDecorView.findViewById(R.id.search_logo);
-        search.setOnClickListener(this);
-        searchEditText = (EditText) mDecorView.findViewById(R.id.search_edittext);
-        searchButton = (Button) mDecorView.findViewById(R.id.search_button);
+
         mTabLayout = mDecorView.findViewById(R.id.tl);
         tl();
         return mDecorView;
@@ -81,27 +72,6 @@ public class FragmentFindJob extends Fragment implements View.OnClickListener{
         });
         vp.setCurrentItem(1);
     }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.search_logo:
-                if(searchEditText.getVisibility()==View.VISIBLE||searchButton.getVisibility()==View.VISIBLE) {
-                    searchButton.setVisibility(View.GONE);
-                    searchEditText.setVisibility(View.GONE);
-                    mTabLayout.setVisibility(View.VISIBLE);
-                } else {
-                    searchButton.setVisibility(View.VISIBLE);
-                    searchEditText.setVisibility(View.VISIBLE);
-                    mTabLayout.setVisibility(View.GONE);
-                }
-                break;
-            case R.id.search_button:
-                //todo 返回查询结果 ->小榕
-                break;
-        }
-    }
-
     private class MyPagerAdapter extends FragmentPagerAdapter {
         public MyPagerAdapter(FragmentManager fm) {
             super(fm);
