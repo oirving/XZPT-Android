c97bed7a526da4dbd15c13a8f9139522c251f8fc
XiaorongKang <XiaorongKang@outlook.com>
2019-05-28 01:48:36 +0800

    feat(历史记录):完成简历生成记录查看

--- app/src/main/java/com/djylrz/xzpt/utils/ResumeModelItem.java
+++ app/src/main/java/com/djylrz/xzpt/utils/ResumeModelItem.java
@@ -1,5 +1,6 @@
 package com.djylrz.xzpt.utils;
 
+import com.djylrz.xzpt.bean.ResumeRecord;
 import com.djylrz.xzpt.bean.ResumeTemplate;
 
 import java.io.Serializable;
@@ -9,6 +10,33 @@ public class ResumeModelItem {
 
     private ResumeTemplate resumeTemplate;
 
+    private ResumeRecord resumeRecord;
+    private String createOrHistory;
+
+    public ResumeModelItem(ResumeRecord resumeRecord) {
+        this.resumeRecord = resumeRecord;
+        this.resumeTemplate=new ResumeTemplate();
+        this.resumeTemplate.setImgFileName(resumeRecord.getTemplateJpgUrl());
+        this.resumeTemplate.setTemplateFileName(resumeRecord.getResumeUrl());
+        this.createOrHistory = PostParameterName.INTENT_PUT_EXTRA_VALUE_RESUME_HISTORY;
+    }
+
+    public ResumeModelItem(int resumeModel) {
+        this.resumeModel = resumeModel;
+    }
+
+    public ResumeModelItem(ResumeTemplate resumeTemplate) {
+        this.resumeTemplate = resumeTemplate;
+    }
+
+    public ResumeRecord getResumeRecord() {
+        return resumeRecord;
+    }
+
+    public void setResumeRecord(ResumeRecord resumeRecord) {
+        this.resumeRecord = resumeRecord;
+    }
+
     public String getCreateOrHistory() {
         return createOrHistory;
     }
@@ -17,29 +45,19 @@ public class ResumeModelItem {
         this.createOrHistory = createOrHistory;
     }
 
-    private String createOrHistory;
-
     public ResumeTemplate getResumeTemplate() {
         return resumeTemplate;
     }
-
 
     public void setResumeTemplate(ResumeTemplate resumeTemplate) {
         this.resumeTemplate = resumeTemplate;
     }
 
-    public ResumeModelItem(int resumeModel) {
-        this.resumeModel = resumeModel;
-    }
-    public ResumeModelItem(ResumeTemplate resumeTemplate) {
-        this.resumeTemplate = resumeTemplate;
+    public int getResumeModel() {
+        return resumeModel;
     }
 
     public void setResumeModel(int resumeModel) {
         this.resumeModel = resumeModel;
-    }
-
-    public int getResumeModel() {
-        return resumeModel;
     }
 }
