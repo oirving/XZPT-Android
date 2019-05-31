fd59dcdd0f2d61f389de1ba01f6da61814ff9b3d
supermingjun <544002828@qq.com>
2019-05-31 16:42:54 +0800

    切换企业端获取投递者简历列表接口

--- app/src/main/java/com/djylrz/xzpt/fragmentCompany/ComResumeCardFragment.java
+++ app/src/main/java/com/djylrz/xzpt/fragmentCompany/ComResumeCardFragment.java
@@ -1,4 +1,5 @@
 package com.djylrz.xzpt.fragmentCompany;
+
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,11 +12,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 import com.djylrz.xzpt.R;
 import com.djylrz.xzpt.bean.PageData;
 import com.djylrz.xzpt.bean.ResumeDelivery;
@@ -33,7 +32,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 import com.google.gson.reflect.TypeToken;
 import com.vondear.rxtool.view.RxToast;
-import com.vondear.rxui.view.dialog.RxDialog;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -57,18 +55,28 @@ public class ComResumeCardFragment extends Fragment {
     private int currentPage = 1;
     private final int PAGE_SIZE = 10;
     private long limitNum = 9999;
+    private int statusA = -100;
+    private int statusB = 100;
 
     public static ComResumeCardFragment getInstance(String title) {
         ComResumeCardFragment sf = new ComResumeCardFragment();
         sf.mTitle = title;
-        if(title.equals("已拒绝")){
+        if (title.equals("已拒绝")) {
             sf.type = 0;
-        }else if(title.equals("已通过")){
+            sf.statusA = -1;
+            sf.statusB = -1;
+        } else if (title.equals("已通过")) {
             sf.type = 1;
-        }else if(title.equals("面试中")){
+            sf.statusA = 7;
+            sf.statusB = 7;
+        } else if (title.equals("面试中")) {
             sf.type = 2;
-        }else if(title.equals("待审核")){
+            sf.statusA = 3;
+            sf.statusB = 6;
+        } else if (title.equals("待审核")) {
             sf.type = 3;
+            sf.statusA = 1;
+            sf.statusB = 2;
         }
         return sf;
     }
@@ -78,6 +86,15 @@ public class ComResumeCardFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        // 刷新数据
+        resumeDeliveryList.clear();
+        currentPage = 1;
+        initResumes();
+        loadMoreWrapper.notifyDataSetChanged();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -88,7 +105,7 @@ public class ComResumeCardFragment extends Fragment {
         //加载数据
         initResumes();
         LinearLayoutManager layoutManager = new LinearLayoutManager(v.getContext());
-        adapter = new ComResumeDeliveryRecordAdapter(resumeDeliveryList,type,getContext());
+        adapter = new ComResumeDeliveryRecordAdapter(resumeDeliveryList, type, getContext());
         loadMoreWrapper = new LoadMoreWrapper(adapter);
         swipeRefreshLayout = v.findViewById(R.id.swipe_refresh_layout);
         recyclerView.setLayoutManager(layoutManager);
@@ -131,97 +148,73 @@ public class ComResumeCardFragment extends Fragment {
         return v;
     }
 
-    private void initResumes(){
+    private void initResumes() {
         //获取token
-        SharedPreferences preferences = getActivity().getSharedPreferences("token",0);
-        String token = preferences.getString(PostParameterName.TOKEN,null);
+        SharedPreferences preferences = getActivity().getSharedPreferences("token", 0);
+        String token = preferences.getString(PostParameterName.TOKEN, null);
         //组装URL
-        String url = PostParameterName.POST_URL_COMPANY_GET_DELIVER_RECORD + token ;
+        String url = PostParameterName.POST_URL_COMPANY_GET_DELIVER_RECORD + token + "&statusA=" + statusA + "&statusB=" + statusB;
         //
         PageData pageData = new PageData();
         pageData.setCurrentPage(currentPage++);
         pageData.setPageSize(PAGE_SIZE);
         //请求数据
         try {
-            Log.d(TAG, "onCreate: 开始发送json请求"+ url);
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url,new JSONObject(new Gson().toJson(pageData)),
+            Log.d(TAG, "onCreate: 开始发送json请求" + url);
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, new JSONObject(new Gson().toJson(pageData)),
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
-                            Log.d(TAG, "onResponse: 返回"+response.toString());
-                            GsonBuilder builder = new GsonBuilder();
-                            builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
-                                @Override
-                                public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-                                    return new Date(json.getAsJsonPrimitive().getAsLong());
-                                }
-                            });
-                            Gson gson =builder.create();
-                            Type jsonType = new TypeToken<TempResponseData<TempResponseResumeData<List<ResumeDelivery>> >>() {}.getType();
-                            final TempResponseData<TempResponseResumeData<List<ResumeDelivery>> > postResult = gson.fromJson(response.toString(), jsonType);
-                            Log.d(TAG, "onResponse: "+postResult.getResultCode());
-                            if(postResult.getResultCode().equals(200)){
-                                TempResponseResumeData<List<ResumeDelivery>>  resultObject = postResult.getResultObject();
-                                List<ResumeDelivery> resumeDeliveryRecordVOS = resultObject.getContentList();
-                                for (int i = 0; i < resumeDeliveryRecordVOS.size(); ++i) {
-                                    int resumeRecordType = 0;
-                                    switch ((int)resumeDeliveryRecordVOS.get(i).getDeliveryStatus()){
-                                        case -1:
-                                            resumeRecordType = 0;
-                                            break;
-                                        case 0:
-                                            resumeRecordType = -1;
-                                            break;
-                                        case 1:
-                                            resumeRecordType = 3;
-                                            break;
-                                        case 2:
-                                            resumeRecordType = 3;
-                                            break;
-                                        case 3:
-                                            resumeRecordType = 2;
-                                            break;
-                                        case 4:
-                                            resumeRecordType = 2;
-                                            break;
-                                        case 5:
-                                            resumeRecordType = 2;
-                                            break;
-                                        case 6:
-                                            resumeRecordType = 2;
-                                            break;
-                                        case 7:
-                                            resumeRecordType = 1;
-                                            break;
-                                        default:
-                                            resumeRecordType = -1;
-                                            break;
+                            Log.d(TAG, "onResponse: 返回" + response.toString());
+                            try {
+                                if (response.getString(PostParameterName.RESPOND_RESULTCODE).equals("200")) {
+                                    GsonBuilder builder = new GsonBuilder();
+                                    builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
+                                        @Override
+                                        public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                                            return new Date(json.getAsJsonPrimitive().getAsLong());
+                                        }
+                                    });
+                                    Gson gson = builder.create();
+                                    Type jsonType = new TypeToken<TempResponseData<TempResponseResumeData<List<ResumeDelivery>>>>() {
+                                    }.getType();
+                                    final TempResponseData<TempResponseResumeData<List<ResumeDelivery>>> postResult = gson.fromJson(response.toString(), jsonType);
+                                    Log.d(TAG, "onResponse: " + postResult.getResultCode());
+                                    List<ResumeDelivery> resumeDeliveryRecordVOS = postResult.getResultObject().getContentList();
+                                    if (resumeDeliveryRecordVOS != null) {
+                                        for (int i = 0; i < resumeDeliveryRecordVOS.size(); ++i) {
+                                            resumeDeliveryList.add(resumeDeliveryRecordVOS.get(i));
+                                        }
                                     }
-                                    Log.d(TAG, "onResponse: 当前的type为"+ resumeRecordType);
-                                    if(type == resumeRecordType){
-                                        Log.d(TAG, "onResponse: 进入if，当前的type为"+ resumeRecordType);
-                                        resumeDeliveryList.add(resumeDeliveryRecordVOS.get(i));
-                                    }
+                                    getActivity().runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            loadMoreWrapper.notifyDataSetChanged();
+                                        }
+                                    });
+                                } else if (response.getString(PostParameterName.RESPOND_RESULTCODE).equals("2018")) {
+                                    getActivity().runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            loadMoreWrapper.setLoadState(loadMoreWrapper.LOADING_END);
+                                            limitNum = resumeDeliveryList.size();
+                                        }
+                                    });
+                                } else {
+                                    RxToast.error("服务器返回数据异常！错误代码：" + response.getString(PostParameterName.RESPOND_RESULTCODE));
                                 }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                RxToast.error("无法连接服务器！");
                             }
-
-                            getActivity().runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    if(postResult.getResultCode().equals(200)){
-                                        loadMoreWrapper.notifyDataSetChanged();
-                                    }else{
-                                        loadMoreWrapper.setLoadState(loadMoreWrapper.LOADING_END);
-                                        limitNum = resumeDeliveryList.size();
-                                    }
-                                }
-                            });
                         }
                     }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
                     Log.e("TAG", error.getMessage(), error);
-                }});
+                    RxToast.error("无法连接服务器！");
+                }
+            });
             VolleyNetUtil.getInstance().setRequestQueue(getContext().getApplicationContext());//获取requestQueue
             VolleyNetUtil.getInstance().getRequestQueue().add(jsonObjectRequest);//添加request
         } catch (JSONException e) {
@@ -234,6 +227,7 @@ public class ComResumeCardFragment extends Fragment {
 //        }
     }
 }
+
 class TempResponseResumeData<T> {
     private Integer currentPage;
     private Integer numOfPage;
